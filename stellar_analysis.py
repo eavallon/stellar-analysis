6a061e8ac31a6e8e01094b64ee032525fed916ef
Tessa D. Wilkinson <tessadwilkinson@gmail.com>
2016-02-29 14:41:03 -0800

    added defs

--- stellar_analysis.py
+++ stellar_analysis.py
@@ -1,9 +1,76 @@
 """
+CSE 160 Final Project: Stellar Analysis
+Authors: Ellis Avallone and Tessa Wilkinson
 
-This is the start of a program to go through kepler data and plot stuffs. Primarily getting this far just to test the
-fork business on github.
+Research Questions:
+1) Does gathering data from one section of the sky mean we get all the same type of star?
+2) What types of stars (of similar size) in this dataset have the most planets?
+3) Do two stars near each other in the sky mean they are located near each other in space?
+
 """
+import numpy as np
+import matplotlib.pyplot
+import math
+import pandas as pd
+import csv
 
-# TODO: set up definitions to read in file data, sort through data, pull out relevant data, plot data, etc...
-# TODO: talk about whether or not we want to use additional files that connect to this one, or classes
+path =  "/home/tessa/Downloads/kepler.txt"
+def read_csv(path):
 
+    """
+    Read Kepler dataset in path that is in csv format to get out data
+    return: store as list of dictionaries where each dictionary is a star and its properties
+    """
+
+    out_dictionary = []
+    for row in csv.DictReader(open(path)):
+        out_dictionary.append(row) # need to see data in to output stuffs
+
+    # alternate way:
+    # df = pd.DataFrame(path)
+    # name = df['column_name']
+
+    return out_dictionary
+
+
+def get_column_data(path, column_name):
+    """
+    Given a specific column name: return every star in database with parameters in specified column
+    returns: a dictionary where they key is the kicnumber and the values are the parameters in the column
+    """
+
+    array = read_csv(path)
+    out_dictionary = {}
+
+    for star_dictionary in array:
+        for key,parameter in star_dictionary.items():
+            kicnumber = star_dictionary['Kepler ID']
+            if key == column_name and len(parameter) > 0:
+                out_dictionary[kicnumber] = parameter
+
+    # as a check for now!
+    for k,v in out_dictionary.items():
+        print k,v
+
+    return out_dictionary
+
+get_column_data(path, 'Teff')
+
+
+# call specific column definition, store [dictionaries]
+# store into a list based on type definition
+
+# question 1
+# create color magnitude plot
+
+# question 2
+# compare radii of stars to see if they are similar sizes
+# histogram definition
+# histogram analysis
+
+# question 3
+# compare coordinates definition
+# compare coordinates of range to type
+#  compare coordinates to color magnitude plot
+
+#
