1a004c3854cdc16652e0a02997671af9f99e8e7d
Tessa D. Wilkinson <tessadwilkinson@gmail.com>
2016-03-07 15:24:42 -0800

    added get_coordinates def

--- stellar_analysis.py
+++ stellar_analysis.py
@@ -12,9 +12,12 @@ import numpy as np
 import matplotlib.pyplot
 import math
 import csv
+from astropy.coordinates import SkyCoord
+import astropy.units as u
+
 
 # This path will change based on where the data-set is located!
-path =  "kepler.txt"
+path =  "kepler_test50.txt"
 
 
 def read_csv(path):
@@ -33,7 +36,7 @@ def read_csv(path):
 def get_column_data(path, column_name):
     """
     Given a specific column name: return every star in database with parameters in specified column
-    returns: a dictionary where they key is the kicnumber and the values are the parameters in the column
+    returns: a dictionary: the key is the kicnumber of each star and the values are the parameters in the column_name
     """
 
     array = read_csv(path)
@@ -45,27 +48,100 @@ def get_column_data(path, column_name):
             if key == column_name and len(parameter) > 0:
                 out_dictionary[kicnumber] = parameter
 
-    # as a check for now!
-    for k,v in out_dictionary.items():
-        print k,v
+    # as a check!
+    # for k,v in out_dictionary.items():
+    #     print k,v
 
     return out_dictionary
 
-get_column_data(path, 'Teff')
 
+#get_column_data(path, 'Teff')
 
-# call specific column definition, store [dictionaries]
 # store into a list based on type definition
 
 # question 1
 # create color magnitude plot
+""" grab 'E(B-V)' column (I think, otherwise B color and subtract V color) and 'KEP Mag' column
+color magnitude diagram: the x axis in this diagram is made from subtracting the colors (or temperatures) obtained by
+imaging stars with different filters (or color ranges). The blue-color range filter - the visual-color range filter (B-V)
+gives a color index in terms of the visual wavelengths that we can observe. This value ranges from 0 to 2 depending on
+how red the star is. The y axis is in terms of absolute magnitude (or how bright the star truely is, not just how bright
+we see it as), and is inverted on this graph so that as you go to great y values, the absolute magnitude gets smaller in
+number. This is because absolute magnitude is one of those historically backwards in scale. (darn conventions!)
+The final plot will have hotter, bluer stars towards the top left, and cooler, older stars on the bottom right. A line
+connecting these will indicate the 'main sequence' of stars, or the stars that are burning hydrogen as part of the main
+life cycle. This is how we will tell which stars are which.
+"""
 
 # question 2
 # compare radii of stars to see if they are similar sizes
+""" grab 'Radius' column
+"""
 # histogram definition
 # histogram analysis
 
 # question 3
+
+def h_m_s_separator(coordinate):
+    """input: string of from ra or dec coordinates '## ## ##.###'
+    output: list of hour, min, sec values [##, ##, ##.##]
+    """
+    hour = 0
+    min = 0
+    sec = 0
+    print 'in hms_sep, coord:', coordinate
+
+    for index, value in enumerate(coordinate):
+        # all kepler targets will be (+) so this can be omitted
+        if value[0] == '+':
+            value = value[1:]
+
+        if index < 3 and value != ' ':
+            if hour != 0:
+                hour = str(hour) + str(value)
+            else:
+                hour = value
+        elif 3 <= index <= 5 and value != ' ':
+            if min != 0:
+                min = str(min) + str(value)
+            else:
+                min = value
+        elif index > 5 and value != ' ':
+            if sec != 0:
+                sec = str(sec) + str(value)
+            else:
+                sec = value
+
+    try:
+        return (int(hour), int(min), float(sec))
+    except ValueError:
+        return (0, 0, 0)
+
+
+def get_coordinates():
+    """
+    calls the coordinages
+    """
+
+    ra = get_column_data(path, 'RA (J2000)')
+    dec = get_column_data(path, 'Dec (J2000)')
+
+    coords = {}
+    for k,v in ra.items():
+        ra_coordlist = h_m_s_separator(v)
+        for key, value in dec.items():
+            dec_coordlist = h_m_s_separator(value)
+            if k == key:
+                coords[k] = [ra_coordlist, dec_coordlist]
+
+    for ke, ve in coords.items():
+        print 'out get_coord', ke,ve
+
+
+
+get_coordinates()
+
+
 # compare coordinates definition
 # compare coordinates of range to type
 #  compare coordinates to color magnitude plot
